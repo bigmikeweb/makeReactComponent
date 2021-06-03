8a15bbaf293f3a13984010385f9c45216e9ecfd1
Michael Weber <mikeweberbusiness@gmail.com>
2021-06-03 14:33:08 -0400

    slight fixes

--- makeComponent.js
+++ makeComponent.js
@@ -2,39 +2,40 @@ const sh = require("shelljs");
 const toTitleCase = (str) => {
   return str.replace(
     /\w\S*/g,
-    (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    (txt) => txt.charAt(0).toUpperCase() + txt.substr(1)
   );
 };
 const makeFileNames = (name) => {
   const componentName = toTitleCase(name);
   return {
-    component: `${componentName}.js`,
+    component: `${componentName}.jsx`,
     styleSheet: `${name}.css`,
     index: `index.js`,
   };
 };
 
 const makeComponent = (name) => {
-  const lowerName = name.toLowerCase();
-  const { component, styleSheet, index } = makeFileNames(lowerName);
-  const componentVar = component.slice(0, -3);
-  sh.mkdir(componentVar);
-  sh.cd(componentVar);
+  sh.cd("src/components");
+  const { component, styleSheet, index } = makeFileNames(name);
+  const componentVar = component.slice(0, -4);
+  sh.mkdir(name);
+  sh.cd(name);
   sh.touch([component, styleSheet, index]);
-  sh.ShellString(`export {default} from "./${styleSheet}"`).to(index);
+  sh.ShellString(`export {default} from "./${componentVar}"`).to(index);
   sh.ShellString(
     `import "./${styleSheet}"
-const ${componentVar} = ({ children }) => {
+const ${componentVar} = () => {
   return (
     <div>
-      {children}
+      ${componentVar}
     </div>
   )
 }
 
 export default ${componentVar}`
   ).to(component);
-  sh.cd("..");
+
+  sh.cd("../../..");
 };
 
 module.exports = makeComponent;
